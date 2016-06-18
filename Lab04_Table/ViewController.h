7a29e0a56e3d61a10bf436567727cd723af9eade
Luis Diaz <luisdiaz@luiss-imac.local>
2016-06-17 20:52:51 -0500

    Initial Commit

---
+++ Lab04_Table/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lab04_Table
+//
+//  Created by Luis Diaz on 6/17/16.
+//  Copyright © 2016 Luis Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lab04_Table/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lab04_Table
+//
+//  Created by Luis Diaz on 6/17/16.
+//  Copyright © 2016 Luis Diaz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
